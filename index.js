82df8cc14eb5bd5aca21f74c90313cdf81c31403
lonexil <adetonalonex@gmail.com>
2023-03-06 13:41:02 -0600

    assignment file added

---
+++ index.js
@@ -0,0 +1,12 @@
+//assinment 1
+function area(b, h) {
+  return (area = 0.5 * b * h);
+}
+console.log(area(10, 10));
+
+
+//assignment2
+function Calyear(year) {
+  return (Calyear = 365 * year);
+}
+console.log(Calyear(10));
